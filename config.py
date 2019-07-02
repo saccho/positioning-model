f1471b81af15b5577aa9a94492e9bb5f21b74a85
saccho <saccho956@yahoo.co.jpgit config --global user.email saccho956@yahoo.co.jpgit config --global user.email saccho956@yahoo.co.jp>
2019-07-03 04:05:31 +0900

    [update] 特徴量を変更

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
 
 MEASURED_DAY = '20190626'
-DATA_FILE_PATH = os.path.join('data', MEASURED_DAY, 'dataset.csv')
+# DATA_FILE_PATH = os.path.join('data', MEASURED_DAY, 'dataset.csv')
+# DATA_COL_NAMES = [
+#     'Position', 
+#     r'$\it{P_{1, 1}}$', r'$\it{P_{2, 1}}$', r'$\it{P_{3, 1}}$',
+#     r'$\it{P_{1, 2}}$', r'$\it{P_{2, 2}}$', r'$\it{P_{3, 2}}$',
+# ]
+DATA_FILE_PATH = os.path.join('data', MEASURED_DAY, 'y_dataset.csv')
 DATA_COL_NAMES = [
     'Position', 
-    r'$\it{P_{-42, 1}}$', r'$\it{P_{0, 1}}$', r'$\it{P_{42, 1}}$',
-    r'$\it{P_{-42, 2}}$', r'$\it{P_{0, 2}}$', r'$\it{P_{42, 2,}}$',
+    r'$\rm{Re}(\it{y_{1, 1}})$', r'$\rm{Re}(\it{y_{2, 1}})$', r'$\rm{Re}(\it{y_{3, 1}})$',
+    r'$\rm{Re}(\it{y_{1, 2}})$', r'$\rm{Re}(\it{y_{2, 2}})$', r'$\rm{Re}(\it{y_{3, 2}})$',
+    r'$\rm{Im}(\it{y_{1, 1}})$', r'$\rm{Im}(\it{y_{2, 1}})$', r'$\rm{Im}(\it{y_{3, 1}})$',
+    r'$\rm{Im}(\it{y_{1, 2}})$', r'$\rm{Im}(\it{y_{2, 2}})$', r'$\rm{Im}(\it{y_{3, 2}})$',
 ]
 LOG_FILE_PATH = os.path.join('log', f'use_{MEASURED_DAY}_data.log')
