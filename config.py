62359ad2b16489ff389f2eb9f81cc0a67cb46aac
saccho <saccho956@yahoo.co.jp>
2019-08-01 20:24:42 +0900

    [clean] リファクタリング

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
 
 MEASURED_DAY = '20190726'
-# DATA_FILE_PATH = os.path.join('data', MEASURED_DAY, 'dataset.csv')
-# DATA_COL_NAMES = [
-#     'Position', 
-#     r'$\it{P_{1, 1}}$', r'$\it{P_{2, 1}}$', r'$\it{P_{3, 1}}$',
-#     r'$\it{P_{1, 2}}$', r'$\it{P_{2, 2}}$', r'$\it{P_{3, 2}}$',
-# ]
-DATA_FILE_PATH = os.path.join('data', MEASURED_DAY, 'y_dataset.csv')
+DATA_FILE_PATH = os.path.join('data', MEASURED_DAY)
+DATASET_FILE_PATH = os.path.join(DATA_FILE_PATH, 'y_dataset.csv')
 DATA_COL_NAMES = [
     'Position_x', 'Position_y', 
     r'$\rm{Re}(\it{y_{1, 1, 1}})$', r'$\rm{Re}(\it{y_{2, 1, 1}})$', r'$\rm{Re}(\it{y_{3, 1, 1}})$',
@@ -21,4 +16,4 @@ DATA_COL_NAMES = [
 ]
 LOG_FILE_PATH = os.path.join('log', f'use_{MEASURED_DAY}_data.log')
 ROOM_IMAGE_PATH = os.path.join('data', 'room.png')
-FIGURE_SAVE_PATH = os.path.join('data', MEASURED_DAY, 'fig')
+FIGURE_SAVE_PATH = os.path.join(DATA_FILE_PATH, 'fig')
--- load_data.py
+++ load_data.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from logger import get_logger
-from config import DATA_FILE_PATH, DATA_COL_NAMES
+from config import DATASET_FILE_PATH, DATA_COL_NAMES
 
 logger = get_logger(__name__)
 
-def load_data(y_cols=('Position',), isdrop_delay=False, test_size=0.45, is_stratify=True, random_state=0):
-    data_df = load_measured_data(isdrop_delay)
+def load_data(y_cols=('Position',), is_drop_delay=False, test_size=0.45, is_stratify=True, random_state=0):
+    data_df = load_measured_data(is_drop_delay)
 
     logger.debug('split for training and testing')
     X_cols = [c for c in DATA_COL_NAMES if c not in y_cols]
@@ -31,12 +31,12 @@ def load_data(y_cols=('Position',), isdrop_delay=False, test_size=0.45, is_strat
 
     return X_train_df, X_test_df, y_train, y_test
 
-def load_measured_data(isdrop_delay=False):
+def load_measured_data(is_drop_delay=False):
     logger.debug('measured data loading')
-    data_df = pd.read_csv(DATA_FILE_PATH, names=DATA_COL_NAMES)
+    data_df = pd.read_csv(DATASET_FILE_PATH, names=DATA_COL_NAMES)
     logger.debug('done')
     
-    if isdrop_delay == True:
+    if is_drop_delay == True:
         return data_df.drop(columns=DATA_COL_NAMES[4:])
     else:
         return data_df
